30aa3c991ebad0b793c51f742135d513b264f7f2
Ranilton <rasrj@cesar.school>
2026-03-24 15:01:46 -0300

    Atualizações de imagem e de funções que não estavam sendo usadas

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,8 +2,3 @@ function mostrarMensagem() {
   alert("Obrigado por visitar meu portfólio!");
 }
 
-function mostrarDetalhes() {
-  const detalhes = document.getElementById("detalhes-sinalize");
-  detalhes.style.display = detalhes.style.display === "none" ? "block" : "none";
-}
-
